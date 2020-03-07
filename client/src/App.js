3593f97bfc52be8a8e62313793c1dbee14593273
EricVincitore <epv1001@verizon.net>
2020-03-07 11:55:57 -0500

    added reactstrap, learning how to use it

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Login from "./pages/Login"
+import Homepage from "./pages/Homepage";
+import Metagame from "./pages/Metagame";
+import CardDatabase from "./pages/Database";
+import Resources from "./pages/Resources"
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Navbar />
         <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/homepage" component={Homepage} />
+          <Route exact path="/metagame" component={Metagame} />
+          <Route exact path="/cardDatabase" component={CardDatabase} />
+          <Route exact path="/resources" component={Resources} />
           <Route component={NoMatch} />
         </Switch>
       </div>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,49 @@
 import React from "react";
 import {Link} from "react-router-dom"
+
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
 function Navbar() {
   return (
     <div class="navbar-fixed">
