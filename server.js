003a5a108f89dd8b14671fc94e8619abb5fcdb20
EricVincitore <epv1001@verizon.net>
2022-04-10 08:33:03 -0400

    updating mongoDB

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 var User = require('./models/user')
+const uri = process.env.MONGODB_URI;
 
 
 // Define middleware here
