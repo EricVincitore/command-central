818e6a94339304f1d80382e2bea48c27e28e30ca
EricVincitore <epv1001@verizon.net>
2022-04-10 09:16:37 -0400

    testing prodcution build

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use(passport.session());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  // });
 };
 
 const apiRoutes = require("./routes/apiRoutes")
