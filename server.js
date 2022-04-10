306910903ca1da2d1e4249be58c5707a992d2276
EricVincitore <epv1001@verizon.net>
2022-04-10 09:21:27 -0400

    removed unneeded get request

--- server.js
+++ server.js
@@ -28,11 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  // });
+  app.use(express.static("client/build"));
 };
 
 const apiRoutes = require("./routes/apiRoutes")
