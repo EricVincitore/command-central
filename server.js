597c85883fca192ddd0f9b901f9017506c97fc9b
EricVincitore <epv1001@verizon.net>
2022-04-10 09:11:57 -0400

    improved production build

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
 };
 
 const apiRoutes = require("./routes/apiRoutes")
